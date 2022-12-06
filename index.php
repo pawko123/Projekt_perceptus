df23c49350547db94878c7e498c7d30805ee90d0
Paweł Łaskarzewski <gamespabloplay@gmail.com>
2022-12-06 23:07:40 +0100

    Update index.php

--- index.php
+++ index.php
@@ -19,16 +19,18 @@ if((isset($_SESSION['zalogowany']))&&($_SESSION['zalogowany']==true)){
     </form>
     <span style="color: red;">
             <?php
-            $fullurl = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-            if(strpos($fullurl,"blad=pil")==true){
-                echo "Przekroczono ilosc nieudanych logowania. Sprobuj ponownie za 10 minut.";
-                goto blad_wykryty;
+            if (isset($_GET['blad'])) 
+            {
+                $blad = $_GET['blad'];
+                if ($blad="pil") {
+                    echo "Przekroczono ilosc nieudanych logowania. Sprobuj ponownie za 10 minut.";
+                    goto blad_wykryty;
+                } else if ($blad="nl") {
+                    echo "Logowanie nieudane. Prosze sprobowac ponownie.";
+                    goto blad_wykryty;
+                }
+                blad_wykryty:
             }
-            else if(strpos($fullurl,"blad=nl")==true){
-                echo "Logowanie nieudane. Prosze sprobowac ponownie.";
-                goto blad_wykryty;
-            }
-            blad_wykryty:
             ?>
         </span>
     <a href="formularz_problemy.php">Zglos nam swoj problem</a>
